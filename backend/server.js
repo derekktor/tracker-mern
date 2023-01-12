0257ace10f10da4cdd78a2748c61a0934bf8bcdd
Derek <baldka1109bh@gmail.com>
2023-01-12 13:51:11 +0800

    Get habits route

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,10 @@ const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+app.get("/api/habits", (req, res) => {
+    res.json({
+        name: "habitOne"
+    });
+})
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
