af9ad536468bc9035f6b2c5bf57c7f3f8750f992
Derek <baldka1109bh@gmail.com>
2023-01-12 13:55:45 +0800

    Change routes path

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 5001;
 
 const app = express();
 
-app.use("/api/habits", require("../routes/habitRoutes"));
+app.use("/api/habits", require("./routes/habitRoutes"));
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
