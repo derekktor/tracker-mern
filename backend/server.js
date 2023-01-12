22fbc0b20a514da0fd1d688e89afb36723be6ad5
Derek <baldka1109bh@gmail.com>
2023-01-12 15:28:14 +0800

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5001;
 const { errorHandler } = require("./middleware/errorMiddleware");
+const { connectDB } = require("./config/db");
+
+connectDB();
 
 const app = express();
 
