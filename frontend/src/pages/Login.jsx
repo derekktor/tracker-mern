d2cc3d244aac3f3de6704c81056505f6dbc53a3a
Derek <baldka1109bh@gmail.com>
2023-01-14 13:19:01 +0800

    Create layout for Login and Register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const { email, password } = formData;
+
+  function handleInputChange(e) {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+
+  function handleLoginClick(e) {
+    e.preventDefault();
+
+    // Clear inputs
+    setFormData({
+      email: "",
+      password: "",
+    });
+  }
+
+  return (
+    <div>
+      <section>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in!</p>
+      </section>
+
+      <section>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email:
+            </label>
+            <input
+              id="email"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password:
+            </label>
+            <input
+              id="password"
+              value={password}
+              name="password"
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleLoginClick}
+          >
+            Login
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-export default Register
+  const { name, email, password, password2 } = formData;
+
+  function handleInputChange(e) {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+
+  function handleRegisterClick(e) {
+    e.preventDefault();
+
+    // Clear inputs
+    setFormData({
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+    });
+  }
+
+  return (
+    <div>
+      <section>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account!</p>
+      </section>
+
+      <section>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name:
+            </label>
+            <input
+              id="name"
+              value={name}
+              name="name"
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email:
+            </label>
+            <input
+              id="email"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password:
+            </label>
+            <input
+              id="password"
+              value={password}
+              name="password"
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password2" className="form-label">
+              Repeat password:
+            </label>
+            <input
+              id="password2"
+              value={password2}
+              name="password2"
+              onChange={handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleRegisterClick}
+          >
+            Register
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default Register;
