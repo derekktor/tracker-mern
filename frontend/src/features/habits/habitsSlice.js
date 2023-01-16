036b9cbf998765a767aae05b1877ddf4b274989b
Derek <baldka1109bh@gmail.com>
2023-01-16 09:19:30 +0800

    Add createHabit builder cases

--- frontend/src/features/habits/habitsSlice.js
+++ frontend/src/features/habits/habitsSlice.js
@@ -23,10 +23,69 @@ export const createHabit = createAsyncThunk(
   }
 );
 
+/**
+ * @name fetchHabits
+ * @description fetches a list of habits for a user
+ */
+export const fetchHabits = createAsyncThunk(
+  "habits/fetchHabits",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await habitsService.fetchHabits(token);
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+/**
+ * @name deleteHabit
+ * @description fetches a list of habits for a user
+ */
+export const deleteHabit = createAsyncThunk(
+  "habits/fetchHabits",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await habitsService.fetchHabits(token);
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 const habitsSlice = createSlice({
   name: "habits",
   initialState,
-  reducers: {},
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+        .addCase(createHabit.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(createHabit.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.habits.push(action.payload);
+        })
+        .addCase(createHabit.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.message = action.payload;
+        })
+  }
 });
 
+export const {reset} = habitsSlice.actions;
 export default habitsSlice.reducer;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import HabitForm from "../components/HabitForm";
+import Spinner from "../components/Spinner";
+import HabitsList from "../features/habits/HabitsList";
+import { fetchHabits, reset } from "../features/habits/habitsSlice";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const user = useSelector((state) => state.auth.user);
+  const {habits, isLoading, isSuccess, isError, message} = useSelector(state => state.habits);
 
   useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
     if (!user) {
       navigate("/login");
     }
-  }, [navigate, user]);
+
+    dispatch(fetchHabits());
+    
+    return () => {
+      dispatch(reset());
+    }
+  }, [navigate, user, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <div>
